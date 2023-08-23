4a932fc1516fe71b49d0119dc9d6ce73edaa39c5
qyopyteam <143008995+qyopyteam@users.noreply.github.com>
2023-08-23 16:49:50 +0200

    Create guess the number.py

---
+++ guess the number.py
@@ -0,0 +1,19 @@
+import random
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess a number between 1 and 100: "))
+        attempts += 1
+        
+        if guess < number_to_guess:
+            print("Higher!")
+        elif guess > number_to_guess:
+            print("Lower!")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+guess_the_number()
