04b0cacfdedbb8b08d2ed5044b87c1ed552ab91d
qyopy <143008995+qyopyteam@users.noreply.github.com>
2023-10-30 00:32:38 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -17,11 +17,11 @@ def hangman():
         print(display_word)
         
         if "_" not in display_word:
-            print("Congratulations! You guessed the word.")
+            print("Congratulations! You guessed the word. :)")
             break
         
         if attempts == 0:
-            print(f"Sorry, you're out of attempts. The word was '{word_to_guess}'.")
+            print(f"Sorry, you're out of attempts. The word was '{word_to_guess}'. :/")
             break
         
         guess = input("Guess a letter: ").lower()
@@ -30,14 +30,14 @@ def hangman():
             continue
         
         if guess in guessed_letters:
-            print("You've already guessed that letter.")
+            print("You've already guessed that letter. :c")
             continue
         
         guessed_letters.append(guess)
         
         if guess not in word_to_guess:
             attempts -= 1
-            print(f"Wrong guess! You have {attempts} attempts left.")
+            print(f"Wrong guess! You have {attempts} attempts left. :c")
         else:
             print("Correct guess!")
 
